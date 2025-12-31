9d9b4d7ce143f7651cb20dd4ca44be64bb8f909e
tutorialcloudtech <tutorialcloudtech@gmail.com>
2025-12-31 07:30:39 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
 yum install java-21-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.10/bin/apache-tomcat-11.0.10.tar.gz
-tar -zxvf apache-tomcat-11.0.10.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="ccit123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-11.0.10/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-11.0.10/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-11.0.10/webapps/manager/META-INF/context.xml
-sh apache-tomcat-11.0.10/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.15/bin/apache-tomcat-11.0.15.tar.gz
+tar -zxvf apache-tomcat-11.0.15.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.15/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.15/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="ccit123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.15/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.15/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-11.0.15/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-11.0.15/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-11.0.15/webapps/manager/META-INF/context.xml
+sh apache-tomcat-11.0.15/bin/startup.sh
